5212536e78352734012b4e41cf90db60b4eae229
sabari-qwerty <sabarigithub@gmail.com>
2025-09-04 15:40:42 +0530

    [+] Add All endpoints

---
+++ api/endpoints.tsx
@@ -0,0 +1,14 @@
+const endpoints = {
+  workspace: "/knowledgebase",
+  status: "/live/chat-stats-overview/",
+  getContact: "/live/session-filter",
+  updateStatus: "/live/chat-status-update",
+  getAllContactChat: "/chatbot/chatsession",
+  addTag: "/live/chat-add-tags",
+  removeTag: "/live/chat-remove-tag",
+  updateAssignee: "/live/chat-assign-member",
+  viewPage: "/chatbot/recentpage/",
+  search: "/live/filter/getFilterData",
+};
+
+export default endpoints;
