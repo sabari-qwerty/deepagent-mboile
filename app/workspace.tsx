fe22802826ee2b45ed81fa5e61d3f45305923e3c
sabari-qwerty <sabarigithub@gmail.com>
2025-09-04 16:08:45 +0530

    [+] install axios

--- app/workspace.tsx
+++ app/workspace.tsx
@@ -1,7 +1,4 @@
 import { WorkSpaceScreen } from "@/components/screen/work.space.screen";
-import { useAuth } from "@/hooks/useAuth";
-
 export default function Workspace() {
-  const { handleLogout } = useAuth();
   return <WorkSpaceScreen />;
 }
