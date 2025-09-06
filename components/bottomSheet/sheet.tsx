bb2e3a767a2c7e1c8a1468dde83d2c48fc97d341
sabari-qwerty <sabarigithub@gmail.com>
2025-09-06 20:55:20 +0530

    [+] Add bottom-sheet [+] Add Custom Bottom Sheet [+] add Custom Bottom Sheet View

---
+++ components/bottomSheet/sheet.tsx
@@ -0,0 +1,31 @@
+import BottomSheet from "@gorhom/bottom-sheet";
+import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
+import { FC, ReactNode } from "react";
+
+interface CustomBottomSheetProp {
+  ref: React.Ref<BottomSheetMethods>;
+  children: ReactNode;
+  snapPoints: string[] | [];
+}
+export const CustomBottomSheet: FC<CustomBottomSheetProp> = ({
+  ref,
+  children,
+  snapPoints,
+}) => {
+  return (
+    <BottomSheet
+      ref={ref}
+      index={-1}
+      snapPoints={snapPoints}
+      enablePanDownToClose={true}
+      handleStyle={{
+        display: "none",
+      }}
+      backgroundStyle={{
+        backgroundColor: "#00000029",
+      }}
+    >
+      {children}
+    </BottomSheet>
+  );
+};
---
+++ components/bottomSheet/view.tsx
@@ -0,0 +1,51 @@
+import { cn } from "@/lib/utils/cn";
+import { BottomSheetView } from "@gorhom/bottom-sheet";
+import { FC, ReactNode, useRef } from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+interface CustomBottomViewProp {
+  handleClose: () => void;
+  children: ReactNode;
+  className?: string;
+}
+export const CustomBottomSheetView: FC<CustomBottomViewProp> = ({
+  handleClose,
+  children,
+  className,
+}) => {
+  const viewRef = useRef<View>(null);
+  return (
+    <BottomSheetView className="flex w-full h-full ">
+      <SafeAreaView className="  flex-1 w-full h-full ">
+        <View
+          className={cn(` w-full mx-auto h-full  justify-end  `)}
+          onTouchStart={(e) => {
+            const touch = e.nativeEvent;
+
+            viewRef.current?.measure((x, y, width, height, pageX, pageY) => {
+              const touchX = touch.pageX;
+              const touchY = touch.pageY;
+              if (
+                touchX < pageX ||
+                touchX > pageX + width ||
+                touchY < pageY ||
+                touchY > pageY + height
+              ) {
+                handleClose();
+              }
+            });
+          }}
+        >
+          <View
+            ref={viewRef}
+            className="w-full h-fit "
+            style={{ backgroundColor: "#0000" }}
+          >
+            {children}
+          </View>
+        </View>
+      </SafeAreaView>
+    </BottomSheetView>
+  );
+};
