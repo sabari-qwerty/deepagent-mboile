dafd165f0633a0f19d9ce3fc97615adb0133694a
sabari-qwerty <sabarigithub@gmail.com>
2025-09-07 15:19:12 +0530

    [+] Add CuttomButtonForCahtBabule Component for Enhanced Message Interaction [+] Integrate CuttomButtonForCahtBabule in MessageRoute for Image, Video, and Document Handling

---
+++ components/button/CusttomButtonForChatBabule.tsx
@@ -0,0 +1,30 @@
+import { MessagesResponse } from "@/types/type";
+import { FC, ReactNode } from "react";
+import { TouchableOpacity } from "react-native";
+import { value } from "../provider/chat.screen.provider";
+
+interface CuttomButtonForCahtBabuleProps {
+  item: MessagesResponse;
+  message: value;
+  setMessage: (value: value) => void;
+  children: ReactNode;
+  url: string;
+}
+
+export const CuttomButtonForCahtBabule: FC<CuttomButtonForCahtBabuleProps> = ({
+  children,
+  message,
+  setMessage,
+  item,
+  url = "",
+}) => {
+  return (
+    <TouchableOpacity
+      onPress={() => {}}
+      onLongPress={() => {}}
+      className="w-[240px]"
+    >
+      {children}
+    </TouchableOpacity>
+  );
+};
--- components/chat/message/message.route.tsx
+++ components/chat/message/message.route.tsx
@@ -1,32 +1,55 @@
+import { CuttomButtonForCahtBabule } from "@/components/button/CusttomButtonForChatBabule";
+import {
+  ChatScreenContext,
+  ChatScreenContextProp,
+} from "@/components/provider/chat.screen.provider";
 import { MessagesResponse } from "@/types/type";
-import { FC } from "react";
+import { FC, useContext } from "react";
 import { Text } from "react-native";
 import { TextRoute } from "./text.route";
 
 export const MessageRoute: FC<{ item: MessagesResponse }> = ({ item }) => {
+  const { messages, setMessages } = useContext(
+    ChatScreenContext
+  ) as ChatScreenContextProp;
   if (item.blocks[0].text && item.blocks[0].text !== "") {
     return <TextRoute item={item} />;
   }
   switch (item.attachments[0].fileType) {
     case "image":
       return (
-        <>
+        <CuttomButtonForCahtBabule
+          item={item}
+          message={messages}
+          setMessage={setMessages}
+          url={"/preview"}
+        >
           <Text className="text-text-primary">Image</Text>
-        </>
+        </CuttomButtonForCahtBabule>
       );
 
     case "video":
       return (
-        <>
+        <CuttomButtonForCahtBabule
+          item={item}
+          message={messages}
+          setMessage={setMessages}
+          url={"/preview"}
+        >
           <Text className="text-text-primary">Video</Text>
-        </>
+        </CuttomButtonForCahtBabule>
       );
 
     case "document":
       return (
-        <>
+        <CuttomButtonForCahtBabule
+          item={item}
+          message={messages}
+          setMessage={setMessages}
+          url={"/preview"}
+        >
           <Text className="text-text-primary">Document</Text>
-        </>
+        </CuttomButtonForCahtBabule>
       );
 
     default:
